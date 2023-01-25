1d28f225eb6c6f06fbe2edae9fcf30819e7cc4aa
rajmac93 <rajmac93@gmail.com>
2023-01-25 17:25:44 +0100

    creating a shape of app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+			<div className='row'>
+				<Main />
+				<Basket />
+			</div>
+		</div>
+	);
 }
 
 export default App;
